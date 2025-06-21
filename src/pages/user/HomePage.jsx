b79fe1d6134ed4f617b68dd4e962322f2bc82a7e
QuyHoang7703 <hoangvanquy772003@gmail.com>
2025-06-21 09:26:32 +0700

    test ci v3

--- src/pages/user/HomePage.jsx
+++ src/pages/user/HomePage.jsx
@@ -62,6 +62,7 @@ const HomePage = () => {
     <div className="relative">
       {/* Feature movie */}
       <FeatureMovie movies={movieFeatureResponse?.data?.result || []} />
+      <p className="text-white">ALo</p>
 
       {/* Top 10 phim bộ */}
       {/* {isSuccess && recommendMoviesResponse?.data?.length > 0 && (
