6f344f0f2e5550ad987e018f34d716fe468f1317
QuyHoang7703 <hoangvanquy772003@gmail.com>
2025-06-22 14:43:09 +0700

    update redirectUri to login with google

--- src/configuration/configuration.js
+++ src/configuration/configuration.js
@@ -1,6 +1,7 @@
 export const OAuthConfig = {
   clientId:
     "579536374000-2jigrlttod4v2m34f5pvejmiopbfdvin.apps.googleusercontent.com",
-  redirectUri: "http://localhost:5173/oauth2/callback",
+  // redirectUri: "http://localhost:5173/oauth2/callback",
+  redirectUri: "https://emovie.io.vn/oauth2/callback",
   authUri: "https://accounts.google.com/o/oauth2/auth",
 };
